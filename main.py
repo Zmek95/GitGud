bbeb3bef9ddd1ba82868588270646b123bdf0c4f
Ziyad Mekhemer <22456560+Zmek95@users.noreply.github.com>
2022-01-19 10:54:04 -0700

    Added extra argument

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 def multiply(num1, num2, num3):
-    return num1 * num2*num3
-=======
-def multiply(num1, num2):
     """[summary]
 
     Args:
@@ -12,7 +8,7 @@ def multiply(num1, num2):
     Returns:
         [type]: [description]
     """    
-    return num1 * num2
+    return num1 * num2 * num3
 
 
 def add(num1, num2):
@@ -26,4 +22,3 @@ def add(num1, num2):
         [type]: [description]
     """    
     return num1 + num2
->>>>>>> 287ff8b (feat: adds addition function)
