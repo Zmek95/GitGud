ddcb53918349455ff4c1f3e928053f6783c5986b
Ziyad Mekhemer <22456560+Zmek95@users.noreply.github.com>
2022-01-19 10:38:58 -0700

    Added documentation

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ def multiply(num1, num2, num3):
     return num1 * num2*num3
 =======
 def multiply(num1, num2):
+    """[summary]
+
+    Args:
+        num1 ([type]): [description]
+        num2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     return num1 * num2
 
 
 def add(num1, num2):
+    """[summary]
+
+    Args:
+        num1 ([type]): [description]
+        num2 ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     return num1 + num2
 >>>>>>> 287ff8b (feat: adds addition function)
