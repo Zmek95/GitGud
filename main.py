29231340e968018a6eba79c65688b5fa32995675
Ziyad Mekhemer <22456560+Zmek95@users.noreply.github.com>
2022-01-19 10:46:56 -0700

    Changed

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+<<<<<<< HEAD
 def multiply(num1, num2, num3):
     return num1 * num2*num3
+=======
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def add(num1, num2):
+    return num1 + num2
+>>>>>>> 287ff8b (feat: adds addition function)
